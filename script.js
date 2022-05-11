b52a29443e5d1b12a942b0dc2bcef89925ea172d
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-05-11 13:20:41 +0200

    added image

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const form = document.getElementById('trainingInput')
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
-	saveFormInput(input.value.lenght)
+	saveFormInput(input.value.length)
 })
 
 const saveFormInput = () => {
 	let input = document.getElementById('input')
 	let resultaten = document.getElementById('result')
+	let feedback = document.getElementById('feedback')
 
-	if (input.value.lenght > 10) {
-		document.getElementById('result').textContent =
-			' Larissa, je had deze dag beter thuis kunnen blijven ' + input.value
-	} else if (input.value.lenght > 20) {
-		document.getElementById('result').textContent =
-			' Larissa, zo komt die tweede gouden plak er niet hoor' + input.value
+	if (input.value.length > 0 && input.value.length < 75) {
+		feedback.textContent = ' Larissa, je had deze dag beter thuis kunnen blijven '
+		resultaten.textContent = input.value
+	} else if (input.value.length > 75 && input.value.length < 150) {
+		feedback.textContent = ' Larissa, zo komt die tweede gouden plak er niet hoor'
+		resultaten.textContent = input.value
 	} else {
-		resultaten.classList.add('bounce')
-		document.getElementById('result').textContent =
-			' De resultaten zijn nog niet om naar huis te schrijven, maar het is meer dan ik gewend ben van je ' +
-			input.value
+		feedback.textContent =
+			' De resultaten zijn nog niet om naar huis te schrijven, maar het is meer dan ik gewend ben van je '
+		resultaten.textContent = input.value
 	}
+	feedback.focus()
 }
